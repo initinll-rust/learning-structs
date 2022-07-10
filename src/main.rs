b3edca8eb1f8d1a8f1136b89966511aa7c3d86b3
Nitin Londhe <nitin.londhe@genmills.com>
2022-07-11 01:31:39 +0530

    Using struct update syntax

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,40 @@
-#[derive(Debug)]
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
-}
+pub mod user;
+
+use user::User;
 
 fn main() {
+    
     // Creating an instance of the User struct
-    let user1 = User {
-        email: String::from("someone@example.com"),
-        username: String::from("someusername123"),
-        active: true,
-        sign_in_count: 1,
-    };
+    let user1 = build_user("someone1@example.com".to_string(), "someusername1".to_string());
     println!("{:#?}", user1);
 
     // Changing the value in the email field of a User instance
-    let mut user2 = User {
-        email: String::from("someone@example.com"),
-        username: String::from("someusername123"),
-        active: true,
-        sign_in_count: 1,
-    };
+    let mut user2 = build_user("someone2@example.com".to_string(), "someusername2".to_string());
     user2.email = String::from("anotheremail@example.com");
     println!("{:#?}", user2);
+
+    // Creating a new User instance using one of the values from user1
+    let user3 = User{
+        email: String::from("anotheremail@example.com"),
+        username: user1.username,
+        active: user1.active,
+        sign_in_count: user1.sign_in_count
+    };
+    println!("{:#?}", user3);
+
+    // Using struct update syntax 
+    let user4 = User{
+        email: String::from("anotheremail@example.com"),
+        ..user2
+    };
+    println!("{:#?}", user4);
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
 }
