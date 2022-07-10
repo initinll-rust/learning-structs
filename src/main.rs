76b481f977cf8ec26b0da89255936eaacf19c751
Nitin Londhe <nitin.londhe@genmills.com>
2022-07-11 01:20:31 +0530

    Changing the value in the email field of a User instance

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,22 @@ struct User {
 }
 
 fn main() {
+    // Creating an instance of the User struct
     let user1 = User {
         email: String::from("someone@example.com"),
         username: String::from("someusername123"),
         active: true,
         sign_in_count: 1,
     };
-    println!("{:#?}",user1);
+    println!("{:#?}", user1);
+
+    // Changing the value in the email field of a User instance
+    let mut user2 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+    user2.email = String::from("anotheremail@example.com");
+    println!("{:#?}", user2);
 }
