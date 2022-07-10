d0ea55633fece9c91786b7c80533d01f071f4c44
Nitin Londhe <nitin.londhe@genmills.com>
2022-07-11 01:17:53 +0530

    Creating an instance of the User struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#[derive(Debug)]
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+    println!("{:#?}",user1);
 }
